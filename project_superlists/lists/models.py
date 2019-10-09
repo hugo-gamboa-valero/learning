140086b24dd2e86698385a0e486554990d169e3e
Hugo G <hugo_gamboa@hotmail.com>
2019-10-09 23:31:08 +0100

    Added a List table to store users' items.

--- project_superlists/lists/models.py
+++ project_superlists/lists/models.py
@@ -3,5 +3,9 @@ from django.db import models
 # Create your models here.
 class Item(models.Model):
    text = models.TextField(default="")  
+   list = models.ForeignKey("List", default=None)
 
-
+class List(models.Model):
+#   id = models.AutoField(primary_key=True) 
+   pass
+ 
--- project_superlists/lists/tests.py
+++ project_superlists/lists/tests.py
@@ -2,7 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from .models import Item
+from .models import Item, List
 from .views import home_page
 
 # Create your tests here.
@@ -22,15 +22,23 @@ class NewListTest(TestCase):
       response = self.client.post("/lists/new", data={"item_text":"A new list item"})
       self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
    def test_saving_and_retrieving_items(self):
+      list_ = List()
+      list_.save()
+
       first_item = Item()
       first_item.text = "The first list item"
+      first_item.list = list_
       first_item.save()
 
       second_item = Item()
       second_item.text = "The second list item"
+      second_item.list = list_
       second_item.save()
+
+      saved_list = List.objects.first()
+      self.assertEqual(saved_list, list_)
 
       saved_items = Item.objects.all()
       self.assertEqual(saved_items.count(), 2)  
@@ -39,7 +47,9 @@ class ItemModelTest(TestCase):
       second_saved_item = saved_items[1]
 
       self.assertEqual(first_saved_item.text, "The first list item")
+      self.assertEqual(first_saved_item.list, list_) 
       self.assertEqual(second_saved_item.text, "The second list item")
+      self.assertEqual(second_saved_item.list, list_) 
 
 class ListViewTest(TestCase):
    def test_uses_list_template(self):
@@ -47,8 +57,9 @@ class ListViewTest(TestCase):
       self.assertTemplateUsed(response, "lists/list.html") 
 
    def test_displays_all_list_items(self):
-      Item.objects.create(text="itemey 1")
-      Item.objects.create(text="itemey 2")
+      list_ = List.objects.create()
+      Item.objects.create(text="itemey 1", list=list_)
+      Item.objects.create(text="itemey 2", list=list_)
 
       response = self.client.get("/lists/the-only-list-in-the-world/")
 
