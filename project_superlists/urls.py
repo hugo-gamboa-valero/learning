ee69cff9f7a3a0e0b5971d9de1474278b31c6c6b
Hugo G <hugo_gamboa@hotmail.com>
2019-10-10 01:08:08 +0100

    Unique lists with items for each user.

--- project_superlists/urls.py
+++ project_superlists/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import url, include
 
 from project_superlists.lists import views as lists_views
+from project_superlists.lists import urls as list_urls
 
 urlpatterns = [
     url(r"^$", lists_views.home_page, name="home"),
-    url(r"^lists/new$", lists_views.new_list, name="new_list"),
-    url(r"^lists/(\d+)/$", lists_views.view_list, name="view_list"),
-    url(r"^lists/(\d+)/add_item$", lists_views.add_item, name="add_item"),
+    url(r"^lists/", include(list_urls)),  
 ]
